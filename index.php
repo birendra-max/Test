5e21683df220f79f1c09bc2d34b1e00553800de8
vicky <birendrapradhan112@gmail.com>
2025-12-23 14:38:15 +0530

    change the index page

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello i am index page";
+for($i=0;$i<=5;$i++){
+    for($j=0;$j<$i;$j++){
+        echo 2;
+    }
+    echo "\n";
+}
+
+for($i=4;$i>0;$i--){
+    for($j=0;$j<$i;$j++){
+        echo 2;
+    }
+    echo "\n";
+}
